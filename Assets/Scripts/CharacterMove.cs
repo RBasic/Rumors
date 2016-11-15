893f3b01d0fcb98b872787e6810495106b762df7
RBasic <rcham.pro@gmail.com>
2016-11-15 17:21:16 +0100

    gizmo rayon detection + roles persos
    Ajout d'un gizmo pour le rayon de detection + ajout des roles de
    personnages dans la classe character

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -1,28 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterMove : MonoBehaviour {
+public class CharacterMove : MonoBehaviour
+{
 
     Collider[] around;
     public float tempsDeplacement = 5;
     public float rayonDetection = 15;
     // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         around = Physics.OverlapSphere(gameObject.transform.position, rayonDetection);
-        foreach(Collider c in around)
+        foreach (Collider c in around)
         {
             if (c.gameObject.name == "pasAuCourant")
             {
-                Debug.Log("j'ai "+c.gameObject.name+" a portee");
+                Debug.Log("j'ai " + c.gameObject.name + " a portee");
                 StartCoroutine(deplacement(c.transform.position, tempsDeplacement));
             }
         }
-	}
+    }
 
     IEnumerator deplacement(Vector3 pos, float time)
     {
@@ -34,5 +37,10 @@ public class CharacterMove : MonoBehaviour {
             elapsedtime += Time.deltaTime;
             yield return null;
         }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(gameObject.transform.position, rayonDetection);
     }
 }
